7070634f7b7dd26a69313c5299c2d05529e2d2a6
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-06-03 18:16:07 -0400

    Initial Commit
    Initial Commit

---
+++ SystemRecon.ps1
@@ -0,0 +1,10 @@
+﻿Get-WmiObject -class Win32_computerSystem | Select-Object Name | Out-File C:\SystemInfo.txt 
+Get-Date | Out-File -Append C:\SystemInfo.txt
+Get-TimeZone | Out-File -Append C:\SystemInfo.txt
+Get-NetIPConfiguration | Out-File -Append C:\SystemInfo.txt
+Get-ComputerInfo | Out-File -Append C:\SystemInfo.txt
+Get-Process | Out-File -Append C:\SystemInfo.txt
+Get-Service | Out-File -Append C:\SystemInfo.txt
+Get-hotFix | Out-File -Append C:\SystemInfo.txt
+Get-WmiObject -class win32_Product | Out-File -Append C:\SystemInfo.txt
+
