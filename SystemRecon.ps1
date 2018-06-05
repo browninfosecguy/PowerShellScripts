58e9bd4616f50f15df13cacae2b8caa53348d30d
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-06-04 22:23:19 -0400

    Improvements

--- SystemRecon.ps1
+++ SystemRecon.ps1
@@ -1,9 +1,13 @@
-﻿Get-WmiObject -class Win32_computerSystem | Select-Object Name | Out-File C:\SystemInfo.txt 
+﻿Write-Output "*************************************************************" | Out-File C:\SystemInfo.txt
+Write-Output "Name of the System" | Out-File -Append C:\SystemInfo.txt 
+Get-WmiObject -class Win32_computerSystem | Select-Object Name | Out-File -Append C:\SystemInfo.txt 
+Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
 Get-Date | Out-File -Append C:\SystemInfo.txt
 Get-TimeZone | Out-File -Append C:\SystemInfo.txt
 Get-NetIPConfiguration | Out-File -Append C:\SystemInfo.txt
 Get-ComputerInfo | Out-File -Append C:\SystemInfo.txt
-Get-Process | Out-File -Append C:\SystemInfo.txt
-Get-Service | Out-File -Append C:\SystemInfo.txt
+Get-Process | Select-Object Name,Path,ProductVersion,Description | Format-List| Out-File -Append C:\SystemInfo.txt
+Get-Service | Select-Object DisplayName,Status | Sort-Object Status -Descending | Out-File -Append C:\SystemInfo.txt
 Get-hotFix | Out-File -Append C:\SystemInfo.txt
 Get-WmiObject -class win32_Product | Out-File -Append C:\SystemInfo.txt
+ Get-CimInstance -class Win32_Product| Select-Object Name,Vendor,Version | Out-File -Append C:\SystemInfo.txt
