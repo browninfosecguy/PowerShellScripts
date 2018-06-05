75b9b6879bf064154569874bb0e0ec73a2c29064
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-06-05 11:59:29 -0400

    More Improvements

--- SystemRecon.ps1
+++ SystemRecon.ps1
@@ -2,12 +2,25 @@
 Write-Output "Name of the System" | Out-File -Append C:\SystemInfo.txt 
 Get-WmiObject -class Win32_computerSystem | Select-Object Name | Out-File -Append C:\SystemInfo.txt 
 Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
+Write-Output "Date and Time Zone" | Out-File -Append C:\SystemInfo.txt
 Get-Date | Out-File -Append C:\SystemInfo.txt
 Get-TimeZone | Out-File -Append C:\SystemInfo.txt
+Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
+Write-Output "IP Configuration" | Out-File -Append C:\SystemInfo.txt
 Get-NetIPConfiguration | Out-File -Append C:\SystemInfo.txt
+Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
+Write-Output "System Information" | Out-File -Append C:\SystemInfo.txt
 Get-ComputerInfo | Out-File -Append C:\SystemInfo.txt
+Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
+Write-Output "Processes Running on the System" | Out-File -Append C:\SystemInfo.txt
 Get-Process | Select-Object Name,Path,ProductVersion,Description | Format-List| Out-File -Append C:\SystemInfo.txt
+Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
+Write-Output "List of Services on the System (Running and Stopped)" | Out-File -Append C:\SystemInfo.txt
 Get-Service | Select-Object DisplayName,Status | Sort-Object Status -Descending | Out-File -Append C:\SystemInfo.txt
+Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
+Write-Output "List of Paatches Applied to the Server" | Out-File -Append C:\SystemInfo.txt
 Get-hotFix | Out-File -Append C:\SystemInfo.txt
-Get-WmiObject -class win32_Product | Out-File -Append C:\SystemInfo.txt
- Get-CimInstance -class Win32_Product| Select-Object Name,Vendor,Version | Out-File -Append C:\SystemInfo.txt
+Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
+Write-Output "List of Installed Software" | Out-File -Append C:\SystemInfo.txt
+#Get-WmiObject -class win32_Product | Out-File -Append C:\SystemInfo.txt
+Get-CimInstance -class Win32_Product| Select-Object Name,Vendor,Version | Out-File -Append C:\SystemInfo.txt
