e9dc52cf733bce2c3b8d1f26381424ed18826337
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-06-07 19:24:50 -0400

    more improvements
    basically presentation and filenaming

--- SystemRecon.ps1
+++ SystemRecon.ps1
@@ -1,32 +1,48 @@
-﻿Write-Output "*************************************************************" | Out-File C:\SystemInfo.txt
-Write-Output "Name of the System" | Out-File -Append C:\SystemInfo.txt 
-Get-WmiObject -class Win32_computerSystem | Select-Object Name | Out-File -Append C:\SystemInfo.txt 
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "Date and Time Zone" | Out-File -Append C:\SystemInfo.txt
-Get-Date | Out-File -Append C:\SystemInfo.txt
-Get-TimeZone | Out-File -Append C:\SystemInfo.txt
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "IP Configuration" | Out-File -Append C:\SystemInfo.txt
-Get-NetIPConfiguration | Out-File -Append C:\SystemInfo.txt
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "System Information" | Out-File -Append C:\SystemInfo.txt
-Get-ComputerInfo | Out-File -Append C:\SystemInfo.txt
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "Processes Running on the System" | Out-File -Append C:\SystemInfo.txt
-Get-Process | Select-Object Name,Path,ProductVersion,Description | Format-List| Out-File -Append C:\SystemInfo.txt
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "List of Services on the System (Running and Stopped)" | Out-File -Append C:\SystemInfo.txt
-Get-Service | Select-Object DisplayName,Status | Sort-Object Status -Descending | Out-File -Append C:\SystemInfo.txt
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "List of Paatches Applied to the Server" | Out-File -Append C:\SystemInfo.txt
-Get-hotFix | Sort-Object InstalledOn| Out-File -Append C:\SystemInfo.txt
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "List of Installed Software" | Out-File -Append C:\SystemInfo.txt
-#Get-WmiObject -class win32_Product | Out-File -Append C:\SystemInfo.txt
-Get-CimInstance -class Win32_Product| Select-Object Name,Vendor,Version | Out-File -Append C:\SystemInfo.txt
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "NTP Settings" | Out-File -Append C:\SystemInfo.txt
-Get-ItemProperty -path Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters|Out-File -Append C:\SystemInfo.txt
-Write-Output "*************************************************************" | Out-File -Append C:\SystemInfo.txt
-Write-Output "Run Key" | Out-File -Append C:\SystemInfo.txt
-Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run | Out-File -Append C:\SystemInfo.txt
+﻿function format{
+        Write-Output "*************************************************************" | Out-File -Append C:\$computerName`_SystemInfo.txt
+}
+
+$computerName = (Get-WmiObject -class Win32_computerSystem).Name.ToString()
+
+Write-Output "*************************************************************" | Out-File C:\$computerName`_SystemInfo.txt
+Write-Output "Name of the System" | Out-File -Append C:\$computerName`_SystemInfo.txt 
+format
+Get-WmiObject -class Win32_computerSystem | Select-Object Name | Out-File -Append C:\$computerName`_SystemInfo.txt 
+format
+Write-Output "Date and Time Zone" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Get-Date | Out-File -Append C:\$computerName`_SystemInfo.txt
+Get-TimeZone | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Write-Output "IP Configuration" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Get-NetIPConfiguration | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Write-Output "System Information" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Get-ComputerInfo | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Write-Output "Processes Running on the System" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Get-Process | Select-Object Name,Path,ProductVersion,Description | Format-List| Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Write-Output "List of Services on the System (Running and Stopped)" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Get-Service | Select-Object DisplayName,Status | Sort-Object Status -Descending | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Write-Output "List of Paatches Applied to the Server" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Get-hotFix | Sort-Object InstalledOn| Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Write-Output "List of Installed Software" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+#Get-WmiObject -class win32_Product | Out-File -Append C:\$computerName_SystemInfo.txt
+Get-CimInstance -class Win32_Product| Select-Object Name,Vendor,Version | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Write-Output "NTP Settings" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Get-ItemProperty -path Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters|Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Write-Output "Run Key" | Out-File -Append C:\$computerName`_SystemInfo.txt
+format
+Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run | Out-File -Append C:\$computerName`_SystemInfo.txt
