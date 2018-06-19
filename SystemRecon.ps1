312b1cb6cc49027e96a04fc5d76619fdb7f1fecd
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-06-19 17:22:41 -0400

    Added Comments

--- SystemRecon.ps1
+++ SystemRecon.ps1
@@ -62,6 +62,7 @@ format
 Get-Date | Out-File -Append C:\$computerName`_SystemInfo.txt
 Get-TimeZone | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
+#Get Network Adapter Information
 Write-Output "IP Configuration" | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
 Get-NetIPConfiguration | Out-File -Append C:\$computerName`_SystemInfo.txt
@@ -70,14 +71,17 @@ Write-Output "System Information" | Out-File -Append C:\$computerName`_SystemInf
 format
 Get-ComputerInfo | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
+#Get a list of Running Processes
 Write-Output "Processes Running on the System" | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
 Get-Process | Select-Object Name,Path,ProductVersion,Description, Company | ConvertTo-Html -CssUri table.css| Out-File C:\$computerName`_SystemInfo_RunningProcesses.html
 format
+#Get a list of Services
 Write-Output "List of Services on the System (Running and Stopped)" | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
 Get-Service | Select-Object DisplayName,Status | Sort-Object Status -Descending | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
+#Get a list of Patches Applied
 Write-Output "List of Patches Applied to the Server" | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
 Get-hotFix | Sort-Object InstalledOn| Out-File -Append C:\$computerName`_SystemInfo.txt
@@ -89,6 +93,7 @@ Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uni
 Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate |ConvertTo-Html -CssUri table.css| Out-File C:\$computerName`_SystemInfo_InstalledPrograms64Bit.html
 #Get-CimInstance -class Win32_Product| Select-Object Name,Vendor,Version | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
+#Get NTP server setting
 Write-Output "NTP Settings" | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
 Get-ItemProperty -path Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters|Out-File -Append C:\$computerName`_SystemInfo.txt
@@ -110,6 +115,7 @@ else
     Write-Output "The host failed to reach Google.ca" | Out-File -Append C:\$computerName`_SystemInfo.txt
 }
 format
+#Get a List of Ports Listening
 Write-Output "List of Open Ports on the System"|Out-File -Append C:\$computerName`_SystemInfo.txt
 format
 Get-NetTCPConnection -State Listen | Select-Object LocalAddress,LocalPort,State | Sort-Object LocalPort -Descending | Out-File -Append C:\$computerName`_SystemInfo.txt
@@ -117,12 +123,17 @@ format
 Write-Output "List of Unknown Processes Running on the Sytem" | Out-File -Append C:\$computerName`_SystemInfo.txt
 checkProcessVendor | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
+#Get a list of Local Accounts
 Write-Output "List of Local Account on System"|Out-File -Append C:\$computerName`_SystemInfo.txt
 format
 Get-LocalUser| Select-Object Name,Enabled,PasswordExpires,PasswordLastSet,PasswordRequired,AccountExpires | Out-File -Append C:\$computerName`_SystemInfo.txt
 format
+#Get a list of Startup Programs
 Write-Output "List of Startup Programs"|Out-File -Append C:\$computerName`_SystemInfo.txt
 format
 Get-CimInstance -class Win32_StartupCommand | Out-File -Append C:\$computerName`_SystemInfo.txt
+
+
+#Compress Everything and put files in a ZIP Folder.
 Compress-Archive -LiteralPath C:\table.css,C:\$computerName`_SystemInfo.txt,C:\$computerName`_SystemInfo_RunningProcesses.html,C:\$computerName`_SystemInfo_InstalledPrograms32Bit.html,C:\$computerName`_SystemInfo_InstalledPrograms64Bit.html -DestinationPath C:\$computerName`_SystemInfo.zip -Force
 
