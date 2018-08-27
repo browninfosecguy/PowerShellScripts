cb9c5a6cee55c0cc28a5ac8783120d7adb2609ae
browninfosecguy <37906998+browninfosecguy@users.noreply.github.com>
2018-08-26 21:00:03 -0400

    Initial Commit

---
+++ PowerShell-CSharp.ps1
@@ -0,0 +1,13 @@
+﻿$source = @"
+using System;
+class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+     }
+"@
+
+Add-Type -TypeDefinition $source -Language CSharp -OutputAssembly "cat.exe" -OutputType ConsoleApplication
