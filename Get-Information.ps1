ace790fb37d5f8d7ecb628316e6dd441b831028d
Sonny <37906998+browninfosecguy@users.noreply.github.com>
2019-03-15 15:10:02 -0400

    Create Get-Information.ps1

---
+++ Get-Information.ps1
@@ -0,0 +1,13 @@
+<##
+
+This script will be used to collect and display system information.
+This Script should be run from Domain Controller.
+
+>#>
+
+function Get-Information{
+
+    $creds=Get-Credential
+}
+
+
